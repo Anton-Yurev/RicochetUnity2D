8b673f64113da8c31896cb4faa4ba1923db45574
Anton_Yurev1310 <foxs.99@mail.ru>
2026-05-17 21:27:07 +0500

    Version 0.3

--- Assets/Project/Scripts/PriceBox.cs
+++ Assets/Project/Scripts/PriceBox.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class PriceBox : Box
 {
     [SerializeField ]
-    GameObject bonus;
+    GameObject bonus1;
+    [SerializeField]
+    GameObject bonus2;
+    [SerializeField]
+    GameObject bonus3;
+    List<GameObject> list;
+    private void Start()
+    {
+        list = new List<GameObject>(3);
+        list.Add(bonus1);
+        list.Add(bonus2);
+        list.Add(bonus3);
+    }
     public override void BoxGetDamage()
     {
+        GameObject bonus= list[Random.Range(0,3)];
         Instantiate(bonus, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
